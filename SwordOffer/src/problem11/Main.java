c8490d6ace333440026d1f7a861a3cc7664cf358
detectiveboy <372687391@qq.com>
2021-03-08 17:38:13 +0800

    problem11

---
+++ SwordOffer/src/problem11/Main.java
@@ -0,0 +1,11 @@
+package problem11;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+//    int[] array = new int[]{3, 4, 5, 1, 2};
+    int[] array = new int[]{1, 3, 5};
+    System.out.println(solution.minArray(array));
+  }
+}
