e8b0677fc09d5bed4677a9b86f5ca654d4e26da1
detectiveboy <372687391@qq.com>
2021-03-10 01:51:46 +0800

    problem14_1

---
+++ SwordOffer/src/problem14_1/Main.java
@@ -0,0 +1,11 @@
+package problem14_1;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    System.out.println(solution.cuttingRope(2));
+    System.out.println(solution.cuttingRope(3));
+    System.out.println(solution.cuttingRope(10));
+  }
+}
