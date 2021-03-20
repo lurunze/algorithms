7038703e314a7ce5aa92294e36946fe275ebed93
detectiveboy <372687391@qq.com>
2021-03-20 14:41:20 +0800

    problem15

---
+++ SwordOffer/src/problem15/Solution.java
@@ -0,0 +1,16 @@
+package problem15;
+
+/**
+ * @author lurunze
+ */
+public class Solution {
+
+  public int hammingWeight(int n) {
+    int res = 0;
+    while (n != 0) {
+      n = n & (n - 1);
+      res++;
+    }
+    return res;
+  }
+}
