90c6da6f22c4d459a0335895688624aa00497972
rainzee <372687391@qq.com>
2022-03-09 18:13:41 +0800

    problem53_2

---
+++ SwordOffer/src/problem53_2/Solution.java
@@ -0,0 +1,17 @@
+package problem53_2;
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == mid) {
+                left = mid + 1;
+            } else if (nums[mid] > mid) {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
